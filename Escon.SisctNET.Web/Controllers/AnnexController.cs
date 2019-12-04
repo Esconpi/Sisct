a65295ee9fc6818cdbd64ea889c37592b8ad4fcd
CONTABIL 01 <esconpigit@gmail.com>
2019-12-04 18:11:44 -0300

    Criando convenio icms

--- Escon.SisctNET.Web/Controllers/AnnexController.cs
+++ Escon.SisctNET.Web/Controllers/AnnexController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Escon.SisctNET.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -30,7 +27,82 @@ namespace Escon.SisctNET.Web.Controllers
                 var result = _service.FindAll(GetLog(Model.OccorenceLog.Read));
                 return View(result);
 
-            }catch(Exception ex)
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            try
+            {
+                return View();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(Model.Annex entity)
+        {
+            try
+            {
+                entity.Created = DateTime.Now;
+                entity.Updated = entity.Created;
+
+                _service.Create(entity,GetLog(Model.OccorenceLog.Create));
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            try
+            {
+                var result = _service.FindById(id, GetLog(Model.OccorenceLog.Read));
+                return View(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Model.Annex entity)
+        {
+            try
+            {
+                var rst = _service.FindById(id, GetLog(Model.OccorenceLog.Read));
+                entity.Created = rst.Created;
+                entity.Updated = DateTime.Now;
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _service.Delete(id, GetLog(Model.OccorenceLog.Delete));
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
             {
                 return BadRequest(new { erro = 500, message = ex.Message });
             }
--- Escon.SisctNET.Web/Controllers/NcmConvenioController.cs
+++ Escon.SisctNET.Web/Controllers/NcmConvenioController.cs
@@ -1,6 +1,7 @@
 ﻿using Escon.SisctNET.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 
 namespace Escon.SisctNET.Web.Controllers
@@ -8,13 +9,16 @@ namespace Escon.SisctNET.Web.Controllers
     public class NcmConvenioController : ControllerBaseSisctNET
     {
         private readonly INcmConvenioService _service;
+        private readonly IAnnexService _annexService;
 
         public NcmConvenioController(
             INcmConvenioService service,
+            IAnnexService annexService,
             IFunctionalityService functionalityService,
             IHttpContextAccessor httpContextAccessor)
             : base(functionalityService, "NcmConvenio")
         {
+            _annexService = annexService;
             _service = service;
             SessionManager.SetIHttpContextAccessor(httpContextAccessor);
         }
@@ -32,6 +36,82 @@ namespace Escon.SisctNET.Web.Controllers
                 return BadRequest(new { erro = 500, message = ex.Message });
             }
            
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            try
+            {
+                ViewBag.AnnexId = new SelectList(_annexService.FindAll(GetLog(Model.OccorenceLog.Read)), "Id", "Description", null);
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(Model.NcmConvenio entity)
+        {
+            try
+            {
+                entity.Created = DateTime.Now;
+                entity.Updated = entity.Created;
+                _service.Create(entity, GetLog(Model.OccorenceLog.Create));
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            try
+            {
+                var result = _service.FindById(id, GetLog(Model.OccorenceLog.Read));
+                ViewBag.AnnexId = new SelectList(_annexService.FindAll(GetLog(Model.OccorenceLog.Read)), "Id", "Description", null);
+                return View(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Model.NcmConvenio entity)
+        {
+            try
+            {
+                var rst = _service.FindById(id, GetLog(Model.OccorenceLog.Read));
+                entity.Created = rst.Created;
+                entity.Updated = DateTime.Now;
+                _service.Update(entity, GetLog(Model.OccorenceLog.Read));
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _service.Delete(id, GetLog(Model.OccorenceLog.Delete));
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { erro = 500, message = ex.Message });
+            }
         }
     }
 }
