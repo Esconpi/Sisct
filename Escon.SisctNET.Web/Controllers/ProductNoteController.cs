1fa4b2b060959698b4b911f75b8c3512ce60e173
SisCT <esconpigit@gmail.com>
2020-02-11 10:29:45 -0300

    Ajuste relatorio bebida incentivo

--- Escon.SisctNET.Web/Controllers/ProductNoteController.cs
+++ Escon.SisctNET.Web/Controllers/ProductNoteController.cs
@@ -797,14 +797,19 @@ namespace Escon.SisctNET.Web.Controllers
                             decimal impostoFecop = Convert.ToDecimal(baseIcms * (company.Fecop / 100));
                             ViewBag.ImpostoFecop = Convert.ToDouble(Math.Round(impostoFecop, 2)).ToString("C2", CultureInfo.CurrentCulture).Replace("R$", "");
 
-                            if(type == 7)
+                            decimal? impostoGeral = 0;
+                            if (type == 7)
                             {
                                var productsP = _service.FindByProductsType(notes, typeTaxation);
 
                                 totalIcms = productsP.Select(_ => _.TotalICMS).Sum();
+                                var totalFecop1 = productsP.Select(_ => _.TotalFecop).Sum();
+                                impostoGeral = totalIcms + totalFecop1;
                             }
-
-                            var impostoGeral = totalIcms + totalFecop;
+                            else
+                            {
+                                impostoGeral = totalIcms + totalFecop;
+                            }                            
 
                             decimal? basefunef = impostoGeral - impostoIcms;
                             ViewBag.BaseFunef = Convert.ToDouble(Math.Round(Convert.ToDecimal(basefunef), 2)).ToString("C2", CultureInfo.CurrentCulture).Replace("R$", "");
