253ce088a3e65896560bf9c585689a180a4be17e
SisCT <esconpigit@gmail.com>
2020-03-09 11:50:06 -0300

    Atualizacao

---
+++ Escon.SisctNET.Web/Taxation/Taxation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Escon.SisctNET.Web.Taxation
+{
+    public class Taxation
+    {
+
+    }
+}
