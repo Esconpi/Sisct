3d413121149b14803a3ae8c84cae0d468676bfd0
Francisco das Chagas B. Silva <fsilva@luxfacta.com>
2020-06-17 12:46:10 -0300

    Configuração do ambiente de execução para integração

--- Escon.SisctNET.Web/Program.cs
+++ Escon.SisctNET.Web/Program.cs
@@ -9,14 +9,14 @@ namespace Escon.SisctNET.Web
 
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-            var host = new WebHostBuilder()
-            .UseKestrel()
-                 .UseContentRoot(Directory.GetCurrentDirectory())
-                 .UseIISIntegration()
-                 .UseStartup<Startup>()
-                 .Build();
-                host.Run();
+           CreateWebHostBuilder(args).Build().Run();
+            //var host = new WebHostBuilder()
+            //.UseKestrel()
+            //     .UseContentRoot(Directory.GetCurrentDirectory())
+            //     .UseIISIntegration()
+            //     .UseStartup<Startup>()
+            //     .Build();
+            //    host.Run();
         }
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
