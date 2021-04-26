09d987c31026808e9a90bbd135e2eb1851e79469
PauloPilar <esconpigit@gmail.com>
2021-04-26 17:05:40 -0300

    adicionando relatório

--- Escon.SisctNET.Web/Controllers/EstoqueController.cs
+++ Escon.SisctNET.Web/Controllers/EstoqueController.cs
@@ -84,28 +84,133 @@ namespace Escon.SisctNET.Web.Controllers
 
                 var meses = importPeriod.Months(inicio, fim);
 
+                var produtosTemp = entradas
+                              .Select(_ => _.Cprod)
+                              .Distinct()
+                              .ToList();
+
+                produtosTemp.AddRange(saidas
+                           .Select(_ => _.Cprod)
+                           .Distinct()
+                           .ToList());
+
+                produtosTemp = produtosTemp
+                            .Distinct()
+                            .ToList();
+
                 if (type.Equals("historico"))
-                {
-                   
-                }
-                else if (type.Equals("livro"))
                 {
                     List<List<string>> produtos = new List<List<string>>();
                     List<List<string>> estoque = new List<List<string>>();
 
-                    var produtosTemp = entradas
-                                .Select(_ => _.Cprod)
-                                .Distinct()
+                    foreach (var p in produtosTemp)
+                    {
+                        var ppEntrada = entradas.Where(_ => _.Cprod.Equals(p)).FirstOrDefault();
+
+                        if (ppEntrada == null)
+                        {
+                            var ppSaida = saidas.Where(_ => _.Cprod.Equals(p)).FirstOrDefault();
+
+                            List<string> produto = new List<string>();
+                            produto.Add(ppSaida.Cprod);
+                            produto.Add(ppSaida.Xprod);
+                            produto.Add(ppSaida.Ucom);
+                            produto.Add(ppSaida.Ncm);
+                            produto.Add(ppSaida.Cest);
+
+                            produtos.Add(produto);
+                        }
+                        else
+                        {
+                            List<string> produto = new List<string>();
+                            produto.Add(ppEntrada.Cprod);
+                            produto.Add(ppEntrada.Xprod);
+                            produto.Add(ppEntrada.Ucom);
+                            produto.Add(ppEntrada.Ncm);
+                            produto.Add(ppEntrada.Cest);
+
+                            produtos.Add(produto);
+                        }
+                    }
+
+                    foreach (var mes in meses)
+                    {
+                        for (int dia = 1; dia <= 31; dia++)
+                        {
+                            foreach (var produto in produtos)
+                            {
+                                decimal quantidade = 0, valor = 0, total = 0;
+
+                                var entradaDia = entradas
+                                .Where(_ => _.MesRef.Equals(mes) && Convert.ToInt32(_.Dhemi.ToString("dd")).Equals(dia) && _.Cprod.Equals(produto[0]))
+                                .OrderBy(_ => _.Cprod)
                                 .ToList();
 
-                    produtosTemp.AddRange(saidas
-                               .Select(_ => _.Cprod)
-                               .Distinct()
-                               .ToList());
+                                foreach (var entrada in entradaDia)
+                                {
+                                    List<string> prod = new List<string>();
 
-                    produtosTemp = produtosTemp
-                                .Distinct()
-                                .ToList();
+                                    prod.Add("COMPRA");
+                                    prod.Add(entrada.Cprod);
+                                    prod.Add(entrada.Dhemi.ToString("dd/MM/yyyy"));
+                                    prod.Add(entrada.Nnf);
+                                    prod.Add(entrada.Qcom.ToString());
+                                    prod.Add(entrada.Vuncom.ToString());
+                                    prod.Add(entrada.Vbasecalc.ToString());
+
+                                    quantidade += Convert.ToDecimal(entrada.Qcom);
+                                    total += Convert.ToDecimal(entrada.Vbasecalc);
+                                    valor = Math.Round(total / quantidade, 2);
+                                    estoque.Add(prod);
+                                }
+
+                                var saidaDia = saidas
+                                    .Where(_ => _.MesRef.Equals(mes) && Convert.ToInt32(_.Dhemi.ToString("dd")).Equals(dia) && _.Cprod.Equals(produto[0]))
+                                    .OrderBy(_ => _.Cprod)
+                                    .ToList();
+
+                                foreach (var saida in saidaDia)
+                                {
+                                    List<string> prod = new List<string>();
+
+                                    prod.Add("VENDA");
+                                    prod.Add(saida.Cprod);
+                                    prod.Add(saida.Dhemi.ToString("dd/MM/yyyy"));
+                                    prod.Add(saida.Nnf);
+                                    prod.Add(saida.Qcom.ToString());
+                                    prod.Add(saida.Vuncom.ToString());
+                                    prod.Add(saida.Vbasecalc.ToString());
+
+
+                                    if (quantidade - Convert.ToDecimal(saida.Qcom) <= 0)
+                                    {
+                                        quantidade = 0;
+                                        total = 0;
+                                        valor = 0;
+                                    }
+                                    else
+                                    {
+                                        quantidade -= Convert.ToDecimal(saida.Qcom);
+                                        total -= Convert.ToDecimal(saida.Vbasecalc);
+                                        valor = Math.Round(total / quantidade, 2);
+                                    }
+
+                                    estoque.Add(prod);
+                                }
+
+                                produto.Add(quantidade.ToString());
+                                produto.Add(valor.ToString());
+                                produto.Add(total.ToString());
+                            }
+                        }
+                    }
+
+                    ViewBag.Produtos = produtos.OrderBy(_ => _[0]).ToList();
+                    ViewBag.Estoque = estoque;
+                }
+                else if (type.Equals("livro"))
+                {
+                    List<List<string>> produtos = new List<List<string>>();
 
                     foreach(var p in produtosTemp)
                     {
@@ -152,20 +257,9 @@ namespace Escon.SisctNET.Web.Controllers
 
                                 foreach(var entrada in entradaDia)
                                 {
-                                    List<string> prod = new List<string>();
-
-                                    prod.Add("COMPRA");
-                                    prod.Add(entrada.Cprod);
-                                    prod.Add(entrada.Dhemi.ToString("dd/MM/yyyy"));
-                                    prod.Add(entrada.Nnf);
-                                    prod.Add(entrada.Qcom.ToString());
-                                    prod.Add(entrada.Vuncom.ToString());
-                                    prod.Add(entrada.Vbasecalc.ToString());
-
                                     quantidade += Convert.ToDecimal(entrada.Qcom);
                                     total += Convert.ToDecimal(entrada.Vbasecalc);
                                     valor = Math.Round(total / quantidade, 2);
-                                    estoque.Add(prod);
                                 }
 
                                 var saidaDia = saidas
@@ -175,21 +269,21 @@ namespace Escon.SisctNET.Web.Controllers
 
                                 foreach (var saida in saidaDia)
                                 {
-                                    List<string> prod = new List<string>();
 
-                                    prod.Add("VENDA");
-                                    prod.Add(saida.Cprod);
-                                    prod.Add(saida.Dhemi.ToString("dd/MM/yyyy"));
-                                    prod.Add(saida.Nnf);
-                                    prod.Add(saida.Qcom.ToString());
-                                    prod.Add(saida.Vuncom.ToString());
-                                    prod.Add(saida.Vbasecalc.ToString());
 
-                                    quantidade -= Convert.ToDecimal(saida.Qcom);
-                                    total -= Convert.ToDecimal(saida.Vbasecalc);
-                                    valor = Math.Round(total / quantidade, 2);
-
-                                    estoque.Add(prod);
+                                    if(quantidade - Convert.ToDecimal(saida.Qcom) <= 0)
+                                    {
+                                        quantidade = 0;
+                                        total = 0;
+                                        valor = 0;
+                                    }
+                                    else
+                                    {
+                                        quantidade -= Convert.ToDecimal(saida.Qcom);
+                                        total -= Convert.ToDecimal(saida.Vbasecalc);
+                                        valor = Math.Round(total / quantidade, 2);
+                                    }
+                                    
                                 }
 
                                 produto.Add(quantidade.ToString());
@@ -198,6 +292,8 @@ namespace Escon.SisctNET.Web.Controllers
                             }
                         }
                     }
+
+                    ViewBag.Produtos = produtos.OrderBy(_ => _[0]).ToList();
                 }
 
                 return View();
