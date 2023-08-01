a75bfad6d132683e7bbfb8852d6706cc2f7163ac
Esconpi <esconpigit@gmail.com>
2023-08-01 11:17:12 -0300

    ajustes

---
+++ Escon.SisctNET.Model/TaxRule.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Escon.SisctNET.Model
+{
+    [Table("taxrule")]
+    public class TaxRule : EntityBase
+    {
+        [Display(Name = "Data Inicial")]
+        [DataType(DataType.Date)]
+        public DateTime DateStart { get; set; }
+
+        [Display(Name = "Data Final")]
+        [DataType(DataType.Date)]
+        public DateTime? DateEnd { get; set; }
+    }
+}
