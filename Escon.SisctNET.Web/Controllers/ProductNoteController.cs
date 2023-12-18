cb3325e49ebe95a46902bab537a5411740a028ce
Esconpi <esconpigit@gmail.com>
2023-12-18 09:33:29 -0300

    ajustes

--- Escon.SisctNET.Web/Controllers/ProductNoteController.cs
+++ Escon.SisctNET.Web/Controllers/ProductNoteController.cs
@@ -361,7 +361,7 @@ namespace Escon.SisctNET.Web.Controllers
                     updateProducts.Add(prod);
 
                     if (productType == "Normal" && (prod.Ucom.ToUpper().Equals("UN") || prod.Ucom.ToUpper().Equals("UND") || 
-                        prod.Ucom.ToUpper().Equals("GF") || prod.Ucom.ToUpper().Equals("GR")))
+                        prod.Ucom.ToUpper().Equals("GF") || prod.Ucom.ToUpper().Equals("GR") || prod.Ucom.ToUpper().Equals("QT")))
                     {
                         string aliquot = prod.Picms.ToString(), code = calculation.CodeP(prod.Note.Company.Document, prod.Note.Cnpj, prod.Cprod, prod.Ncm, prod.Note.Uf, aliquot);
                         var taxationcm = _taxationPService.FindByNcm(code, prod.Cest);
